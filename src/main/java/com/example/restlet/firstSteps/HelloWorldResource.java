c797dbc8359b64f22f7446fce706b14d83d84e0f
Fedor Fedoseev <fedorfdsv@gmail.com>
2016-11-14 02:59:37 +0100

    guestbook.xsd created

--- src/main/java/com/example/restlet/firstSteps/HelloWorldResource.java
+++ src/main/java/com/example/restlet/firstSteps/HelloWorldResource.java
@@ -11,7 +11,19 @@ public class HelloWorldResource extends ServerResource {
 
     @Get
     public String represent() {
-        return "hello, world (from the cloud!)";
+        String guestbook_xml = ""
+            + "<guestbook\n"
+            + "xmlns=\"http://example.com\"\n"
+            + "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n"
+            + "xsi:schemaLocation=\"http://example.com guestbook.xsd\">\n"
+            + "    <greeting>\n"
+            + "        <user>Nfan</user>\n"
+            + "        <message>Hello World!</message>\n"
+            + "    </greeting>\n"
+            + "</guestbook>\n";
+
+        // return "hello, world (from the cloud!)";
+        return guestbook_xml;
     }
 
 }
